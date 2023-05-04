224bf41ef5783103cccaef3c0cfa83c375c896c7
oscarroa8 <oscaroalesgonzalez@gmail.com>
2023-05-04 18:20:49 +0200

    estructuracion basica

---
+++ proyectoPadel/src/scripts/scripts.sql
@@ -0,0 +1,44 @@
+drop table if EXISTS pista;
+
+drop table if EXISTS usuario;
+
+drop table if EXISTS cliente;
+
+drop table if EXISTS cliente_pista;
+
+create table pista(
+    idPista integer primary key AUTOINCREMENT not null,
+    nombre text,
+    material text,
+    precio real,
+    idUsuario integer,
+    FOREIGN KEY(idUsuario) REFERENCES usuario(idUsuario)
+);
+
+create table usuario(
+    idUsuario integer primary key AUTOINCREMENT not null,
+    nombre text,
+    apellido1 text,
+    apellido2 text,
+    email text,
+    contrasena text
+);
+
+create table cliente(
+    idCliente integer primary key AUTOINCREMENT not null,
+    nombre text,
+    apellido1 text,
+    apellido2 text,
+    email text,
+    contrasena text
+);
+
+create table cliente_pista(
+    idPista integer,
+    idCliente integer,
+    fecha text,
+    horainicio text,
+    horafin text,
+    preciototal real,
+    PRIMARY key (idPista, idCliente)
+);
